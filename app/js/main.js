50420e52e943ebb25918deee9a42ac6bd8ee3d15
Rad Poznyakov <rad.poznyakov@gmail.com>
2017-12-09 17:02:23 +0200

    intro section

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,23 @@
-$(function (){
-  var initialState = {
-    heightWindow: $(window),
-    introPage: $('#intro-page'),
-
-    render: function() {
-      var windowState = {
-        height: this.heightWindow.height(),
-      };
-      try{
-        this.introPage.height(windowState.height);
-
-      }catch(e){
-        console.log(e);
-      }
-    }
-  }
-  initialState.render();
-  $(window).resize(function(){
-    initialState.render();
-  })
-});
+// $(function (){
+//   var initialState = {
+//     heightWindow: $(window),
+//     introPage: $('#intro-page'),
+//
+//     render: function() {
+//       var windowState = {
+//         height: this.heightWindow.height(),
+//       };
+//       try{
+//         this.introPage.height(windowState.height);
+//
+//       }catch(e){
+//         console.log(e);
+//       }
+//     }
+//   }
+//   initialState.render();
+//   $(window).resize(function(){
+//     initialState.render();
+//   })
+// });
 //git
